649ce25ba42e726597014c76db897f110d8106ae
Jay Jackson <jay.liveb@gmail.com>
2022-10-26 08:15:30 -0400

    feat: updates

--- backend/osm_processor.py
+++ backend/osm_processor.py
@@ -3,7 +3,7 @@ from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
 from OSMPythonTools.nominatim import Nominatim
 from OSMPythonTools.data import Data, dictRangeYears, ALL
 from collections import OrderedDict
-import re, pprint
+import re, pprint, time
 import osm2geojson
 import geojson
 import geopandas as gpd
@@ -25,6 +25,21 @@ class OSMQueryResult:
 
     def geojson(self):
         return osm2geojson.json2geojson(self._overpass_query.toJSON())
+
+    def summary(self):
+        geometry = None
+        for elem in self._overpass_json['elements']:
+            elem_type = elem.get("type")
+            if elem_type and elem_type == "node":
+                print(elem.get("tags"))
+                # geometry = geojson.Point((elem.get("lon"), elem.get("lat")))
+                # feature = geojson.Feature(id=elem['id'],
+                #                           geometry=geometry,
+                #                           properties=elem.get("tags"))
+
+    def write(self):
+        with open("response-latest.geojson", 'w') as outfile:
+            geojson.dump(self.geojson(), outfile, indent=4)
 
     def geojson_nodes(self):
         features = []
@@ -63,41 +78,53 @@ class OSMQueryResult:
 
 if __name__ == "__main__":
 
-    lat_max,lon_min,lat_min,lon_max = 47.617581, 7.606412, 47.586086, 7.668244
-    overpass_query = '''
-    (
+    # lat_max,lon_min,lat_min,lon_max = 47.617581, 7.606412, 47.586086, 7.668244
+    # overpass_query = '''
+    # (
 
-    way[highway=motorway]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway= trunk]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=primary]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=secondary]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=tertiary]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=unclassified]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=residential]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=service]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=motorway_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=trunk_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=primary_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=secondary_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=tertiary_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=unclassified_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=residential_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=service_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=living_street]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=track]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=escape]({lat_min}, {lon_min}, {lat_max}, {lon_max});
-    way[highway=road]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=motorway]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway= trunk]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=primary]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=secondary]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=tertiary]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=unclassified]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=residential]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=service]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=motorway_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=trunk_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=primary_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=secondary_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=tertiary_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=unclassified_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=residential_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=service_link]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=living_street]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=track]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=escape]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # way[highway=road]({lat_min}, {lon_min}, {lat_max}, {lon_max});
+    # );
+    # out body;
+    # >;
+    # out skel qt;
+    # '''.format(lat_min=lat_min,
+    #            lon_min=lon_min,
+    #            lat_max=lat_max,
+    #            lon_max=lon_max)
+
+    overpass_query = """
+    (
+        node["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
+        way["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
+        relation["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
     );
     out body;
     >;
     out skel qt;
-    '''.format(lat_min=lat_min,
-               lon_min=lon_min,
-               lat_max=lat_max,
-               lon_max=lon_max)
+    """
 
-
-    print(OSMQueryResult().execute(overpass_query).geojson())
+    result = OSMQueryResult().execute(overpass_query)
+    print(result.summary())
+    result.write()
 
 
 # result = OSMQueryResult().execute("""
