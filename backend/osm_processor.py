32173a3d1688be4839e498d61736820d84f19d00
Jay Jackson <jay.liveb@gmail.com>
2022-10-31 22:50:31 -0400

    feat: updates

--- backend/osm_processor.py
+++ backend/osm_processor.py
@@ -1,24 +1,60 @@
 # OSM Query Processor
 from OSMPythonTools.overpass import Overpass, overpassQueryBuilder
 from OSMPythonTools.nominatim import Nominatim
+from OSMPythonTools.api import Api
 from OSMPythonTools.data import Data, dictRangeYears, ALL
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import re, pprint, time
 import osm2geojson
 import geojson
 import geopandas as gpd
+from cachetools import LRUCache, cachedmethod
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+import xml.etree.ElementTree as ET
 
-class OSMQueryResult:
+cachesize = 1000
+
+
+class OSMService:
+    _cache = LRUCache(maxsize=cachesize)
     _geometry = None
     _overpass_query = None
     _overpass_json = None
 
-    def __init__(self): pass
+    def __init__(self):
+        pass
+
+    @cachedmethod(lambda self: self._cache)
+    def osmByIdSummary(self, type, id):
+        tags = {}
+        properties = {}
+        api = Api()
+        node = api.query('%s/%s' % (str(type), str(id)))  # 'way/270883815'
+        root = ET.fromstring(node.toXML())
+        # print(node.toXML())
+        root_way = root.find("way")
+        root_node = root.find("node")
+        root_relation = root.find("relation")
+        if root_way: properties = root_way.attrib
+        if root_node: properties = root_node.attrib
+        if root_relation: properties = root_relation.attrib
+        for elem in root.findall('.//way/tag'):
+            tags[elem.get('k')] = elem.get('v')
+        for elem in root.findall('.//node/tag'):
+            tags[elem.get('k')] = elem.get('v')
+        for elem in root.findall('.//relation/tag'):
+            tags[elem.get('k')] = elem.get('v')
+        return dict(tags=tags, properties=properties)
+
+    @cachedmethod(lambda self: self._cache)
     def execute(self, query, timeFilter=None):
         # NOTE: Remove [out:json][timeout:25]; from query
-        # TODO: Should we try/retry a few times based on cache or timeouts on big queries
+        # NOTE: Should we try/retry a few times based on cache or timeouts on big queries
         self._geometry = ""
-        self._overpass_query = Overpass().query(query, timeout=100)
+        self._overpass_query = Overpass().query(query,
+                                                shallow=False,
+                                                timeout=100)
         self._overpass_json = self._overpass_query.toJSON()
         # json2shapes = osm2geojson.json2shapes(result.toJSON())
         return self
@@ -27,19 +63,45 @@ class OSMQueryResult:
         return osm2geojson.json2geojson(self._overpass_query.toJSON())
 
     def summary(self):
-        geometry = None
+        summary = {
+            "total_element_count": 0,  # Quantity of entities
+            "total_way_count": 0,
+            "total_node_count": 0,
+            "total_relation_count": 0,
+            # Quantity of users who created or modified entities
+            "total_user_modified_count": 0,
+            "user_modified_count": defaultdict(int)
+            # Change over time for above
+        }
+        merged_tags = defaultdict(list)
+        merged_elements = defaultdict(list)
         for elem in self._overpass_json['elements']:
             elem_type = elem.get("type")
-            if elem_type and elem_type == "node":
-                print(elem.get("tags"))
-                # geometry = geojson.Point((elem.get("lon"), elem.get("lat")))
-                # feature = geojson.Feature(id=elem['id'],
-                #                           geometry=geometry,
-                #                           properties=elem.get("tags"))
+            elem_id = elem.get("id")
+            lookup = self.osmByIdSummary(elem_type, elem_id)
+            summary["total_element_count"] += 1
+            user = lookup["properties"].get("user", None)
+            if user:
+                summary["total_user_modified_count"] += 1
+                summary["user_modified_count"][user] += 1
+            summary["total_%s_count" % (elem_type)] += 1
+            if elem.get("tags"):
+                for k, v in elem.get("tags").items():
+                    if v not in merged_tags[k]:
+                        merged_tags[k].append(v)
+                    merged_elements[str("=".join([k, v]))].append(
+                        str("%s/%s" % (elem_type, elem_id)))
+        return dict(
+            summary=dict(summary),
+            merged_tags=dict(merged_tags),
+            merged_elements=dict(merged_elements),
+        )
 
     def write(self):
-        with open("response-latest.geojson", 'w') as outfile:
+        with open("osm-response-latest.geojson", 'w') as outfile:
             geojson.dump(self.geojson(), outfile, indent=4)
+        with open("osm-response-summary.json", 'w') as outfile:
+            geojson.dump(self.summary(), outfile, indent=4)
 
     def geojson_nodes(self):
         features = []
@@ -48,7 +110,9 @@ class OSMQueryResult:
             elem_type = elem.get("type")
             if elem_type and elem_type == "node":
                 geometry = geojson.Point((elem.get("lon"), elem.get("lat")))
-                feature = geojson.Feature(id=elem['id'], geometry=geometry, properties=elem.get("tags"))
+                feature = geojson.Feature(id=elem['id'],
+                                          geometry=geometry,
+                                          properties=elem.get("tags"))
                 features.append(feature)
         return geojson.FeatureCollection(features)
 
@@ -62,19 +126,16 @@ class OSMQueryResult:
             if elem_type and elem_type == "way":
                 coords = []
                 for node_id in elem['nodes']:
-                    pnt = all_nodes[all_nodes['id'] == node_id]['geometry'].iloc[0]
+                    pnt = all_nodes[all_nodes['id'] ==
+                                    node_id]['geometry'].iloc[0]
                     coords.append((pnt.x, pnt.y))
                 geometry = geojson.LineString(coords)
-                feature = geojson.Feature(id=elem['id'], geometry=geometry, properties=elem.get("tags"))
+                feature = geojson.Feature(id=elem['id'],
+                                          geometry=geometry,
+                                          properties=elem.get("tags"))
                 features.append(feature)
         return geojson.FeatureCollection(features)
-        # if json2shapes:
-        #     shapes = []
-        #     for shape_obj in json2shapes:
-        #         shapes.append(shape_obj.get('shape'))
-        #         # summarize the json..
-        #         # - tag summary counts
-        #         # - node/way/relation count
+
 
 if __name__ == "__main__":
 
@@ -114,46 +175,14 @@ if __name__ == "__main__":
     overpass_query = """
     (
         node["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
-        way["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
         relation["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
+        way["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
     );
     out body;
     >;
     out skel qt;
     """
-
-    result = OSMQueryResult().execute(overpass_query)
-    print(result.summary())
+    # print(overpass_query)
+    result = OSMService().execute(overpass_query)
     result.write()
-
-
-# result = OSMQueryResult().execute("""
-# (
-#     node["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
-#     way["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
-#     relation["brand:wikidata"="Q37158"]["amenity"="cafe"](40.69977176830021,-74.04047012329102,40.790679480243526,-73.9156723022461);
-# );
-# out body;
-# >;
-# out skel qt;
-# """)
-# print(result.geojson_ways())
-
-
-# print(re.sub(r'(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)', '', query))
-# Fetches current OpenStreetMap entities that match given tags in
-#   a given geographic area (e.g. using the Overpass API)
-# RETURN:
-# Computes statistics on the data including:
-# Quantity of entities
-# Quantity of users who created or modified entities
-# Change over time for above
-# Writes statistics to file in a standard data format
-# Writes entities to file in a standard geospatial data format (e.g. GeoJSON)
-# Upload file to S3 or other destination
-# OUTPUT: Config/MapModel
-# MapModel: {
-# - summaryDataLocations
-# - geoJSON map data
-# - summary data (# of entities, # of tags, aggregations, counts, etc.)
-# }
+    # print(result.summary())
